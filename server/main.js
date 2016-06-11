9063fc99a1deb91f34f3b5fd564a9aa42d0eeaf9
heartziq <heartziq@gmail.com>
2016-06-11 14:49:32 +0800

    latest

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 //import all collections from api here
-// import '../imports/api/collections.js';
+import '../imports/api/events.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
